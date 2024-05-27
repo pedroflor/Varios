7def19e87926a2d592217ecdcbda7ffb903f2de2
PF <pedro.flor@gmail.com>
2024-05-27 14:12:01 -0400

    Update f-n-r.sh

--- f-n-r.sh
+++ f-n-r.sh
@@ -10,5 +10,5 @@ do
     sed -i -E 's/<section class="sect3.*" title=.+>/<section class="sect3" title="">/g' $xhtmlfile
     sed -i -E 's/<section class="sect4.*" title=.+>/<section class="sect4" title="">/g' $xhtmlfile
     sed -i -E 's/<section class="sect5.*" title=.+>/<section class="sect5" title="">/g' $xhtmlfile
-    sed -i 's/PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZlcnNpb249JzEuMScgaGVpZ2h0PSc3NDMuNzVweCcgd2lkdGg9JzEwMHB4Jz48dGV4dCB0cmFuc2Zvcm09J3RyYW5zbGF0ZSgyMCwgMTApIHJvdGF0ZSg5MCknIGZpbGw9J3JnYigxOTcsMTk3LDE5NyknIGZvbnQtc2l6ZT0nMTRweCcgZm9udC1mYW1pbHk9J21vbm9zcGFjZSc+Rm9yIHVzZSBieSBQZWRybyBGbG9yIHBlZHJvLmZsb3IgcGVkcm8uZmxvckBnbWFpbC5jb20gQ29weXJpZ2h0IOKSuCAyMDIzIFJlZCBIYXQsIEluYy48L3RleHQ+PC9zdmc+/PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZlcnNpb249JzEuMScgaGVpZ2h0PSc3NDMuNzVweCcgd2lkdGg9JzEwMHB4Jz48dGV4dCB0cmFuc2Zvcm09J3RyYW5zbGF0ZSgyMCwgMTApIHJvdGF0ZSg5MCknIGZpbGw9J3JnYigxOTcsMTk3LDE5NyknIGZvbnQtc2l6ZT0nMTRweCcgZm9udC1mYW1pbHk9J21vbm9zcGFjZSc+Rm9yIHVzZSBieSBKdWFuIFAuIGp1YW5wLnJoY2lAZ21haWwuY29tIENvcHlyaWdodCDikrggMjAyNCBSZWQgSGF0LCBJbmMuPC90ZXh0Pjwvc3ZnPgo=/g' $xhtmlfile
+    sed -i 's/BASE64_ORIGINAL/BASE64_NUEVA/g' $xhtmlfile
 done
