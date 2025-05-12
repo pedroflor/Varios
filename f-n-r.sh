fafae60e3087c99dc2f67a51c668aad6d197bedf
PF <pedro.flor@gmail.com>
2025-05-11 23:26:18 -0400

    se añadieron más titles

--- f-n-r.sh
+++ f-n-r.sh
@@ -12,5 +12,11 @@ do
     sed -i -E 's/<section class="sect3.*" title=.+>/<section class="sect3" title="">/g' $xhtmlfile
     sed -i -E 's/<section class="sect4.*" title=.+>/<section class="sect4" title="">/g' $xhtmlfile
     sed -i -E 's/<section class="sect5.*" title=.+>/<section class="sect5" title="">/g' $xhtmlfile
+
+    sed -i -E 's/title="Nota"/title="">/g' $xhtmlfile
+    sed -i -E 's/title="Referencias"/title="">/g' $xhtmlfile
+    sed -i -E 's/title="Importante"/title="">/g' $xhtmlfile
+    sed -i -E 's/title="Advertencia"/title="">/g' $xhtmlfile
+
     sed -i 's/BASE64_ORIGINAL/BASE64_NUEVA/g' $xhtmlfile
 done
